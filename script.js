a4a4d2d81a70de689bff57fad6422a539a48f2f3
VesnaBozic <vesna.bozic.se@gmail.com>
2024-10-16 12:17:34 +0200

    added chair

---
+++ script.js
@@ -0,0 +1,32 @@
+import {
+  ViewerApp,
+  AssetManagerPlugin,
+  addBasePlugins,
+  MaterialConfiguratorPlugin
+} from "https://dist.pixotronics.com/webgi/runtime/bundle-0.9.3.mjs";
+
+async function setupViewer() {
+  // Initialize the viewer
+  const viewer = new ViewerApp({
+    canvas: document.getElementById("mcanvas")
+  });
+
+  // Add all the plugins at once
+  await addBasePlugins(viewer);
+
+  await viewer.addPlugin(MaterialConfiguratorPlugin)
+
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+  // // This must be called after adding any plugin that changes the render pipeline.
+  // viewer.renderer.refreshPipeline();
+
+  // // Load a 3d model configured in the webgi editor using MaterialConfiguratorPlugin
+  await viewer.load(
+    "https://rio-assets.s3.eu-west-2.amazonaws.com/furniture/chair.glb"
+  );
+
+  // await manager.addFromPath("https://rio-assets.s3.eu-west-2.amazonaws.com/digital-printing-3d-design/igor-boxes.glb");
+
+}
+
+setupViewer();
